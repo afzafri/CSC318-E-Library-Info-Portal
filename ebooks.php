999dca83857216d3825a8fa3edc898e0249ea811
Afif Zafri <zafri_21596@yahoo.com>
2016-09-20 08:54:51 +0800

    E-Library Info Portal
    Interactive and Responsive web application and website

---
+++ ebooks.php
@@ -0,0 +1,25 @@
+<?php 
+	$titlepage = "E-Books";
+	include "./template/header.php";
+?>
+
+<div id="EbooksListPage"></div>
+
+<div class="container">
+	<div class="panel panel-success">
+		<div class="panel-body">
+				<div class="page-header">
+		 		 	<h1>E-Books</h1>
+				</div>
+				<p>Free E-Books are available at E-Library Info portal. You can read it online in the portal, or download it to your devices.</p>
+
+				
+				  <div id="listAllEbooks"></div>
+				
+			<br><br><br>
+		</div>	
+	</div>
+	<br><br><br>
+</div>
+
+<?php include "./template/footer.php"; ?>
---
+++ template/footer.php
@@ -0,0 +1,52 @@
+<div class="overlay">
+  <div class="popup">
+    <div class="sk-circle">
+      <div class="sk-circle1 sk-child"></div>
+      <div class="sk-circle2 sk-child"></div>
+      <div class="sk-circle3 sk-child"></div>
+      <div class="sk-circle4 sk-child"></div>
+      <div class="sk-circle5 sk-child"></div>
+      <div class="sk-circle6 sk-child"></div>
+      <div class="sk-circle7 sk-child"></div>
+      <div class="sk-circle8 sk-child"></div>
+      <div class="sk-circle9 sk-child"></div>
+      <div class="sk-circle10 sk-child"></div>
+      <div class="sk-circle11 sk-child"></div>
+      <div class="sk-circle12 sk-child"></div>
+     </div>
+  </div>
+</div>
+
+<!-- footer -->
+<footer class="footer">
+
+  <span style="float:left;">
+  	<font size="5px"><b>Contact</b></font><br>
+  	Muhammad Afif Zafri Bin Khuzairi<br>
+    Hassan Bin Abdul Hamid<br>
+    Hussin Bin Abdul Hamid<br>
+  </span>
+
+  <span style="float:right;">
+  <center>
+	  <b>&nbsp; Copyright &copy; 2016  </b>
+	  <br>
+
+	  <div class="socials-link">
+		  <a href="http://www.facebook.com/afzafri" title="Find us on Facebook" target="_blank" class="socials-link-fb"><i class="fa fa-facebook-square" alt="Find us on Facebook"></i><span class="sr-only">Facebook&gt;/span&gt;</span></a> 
+
+	      <a href="https://twitter.com/afzafri" title="Follow us on Twitter" target="_blank" class="socials-link-twitter"><i class="fa fa-twitter-square" alt="Follow us on Twitter"></i><span class="sr-only">Twitter</span></a>
+		  
+		   <a href="https://github.com/afzafri/" title="Follow us on GitHub" target="_blank" class="socials-link-github"><i class="fa fa-github-square" alt="Follow us on GitHub"></i><span class="sr-only">GitHub</span></a>
+      </div>
+      </center>
+  </span>
+
+</footer>
+
+
+</body>
+
+
+
+</html>
